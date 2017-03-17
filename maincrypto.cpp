bfecd13c0fbb7cc612d4f1b54db21493e6f3a702
Artem Lipatkin <lipatkin96@gmail.com>
2017-03-17 20:58:12 +0700

    class SequenceConversation can converse one sequence to another

--- maincrypto.cpp
+++ maincrypto.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 
-#include "SequenceGenerator.h"
+#include "SequenceConversation.h"
 
 using namespace std;
 using namespace PseudoRandomSequences;
 
 //TODO: try to use GoogleTests
 int main(void) {
-	SequenceGenerator seq(3);
+	time_t t;
+	srand((unsigned)time(&t));
 
-	std::cout << seq << std::endl;
-	//std::cout << seq.get(0, 0) << std::endl;
-	seq.save("BinaryMatrixGenerator.txt");
+
+
+	//string filename = "BinaryMatrixGenerator.txt";
+	const uint32 k = 3;
+	SequenceConversation Matrix(k);
+	const uint32 N = 30;
+	Sequence seq(N);
+
+	//possibility(1) = 1 / 10
+	for (size_t i = 0; i < seq.size(); i++) {
+		seq[i] = (0 == std::rand() % 10) ? 1 : 0;
+	}
+	cout << seq << endl;
+	cout << Matrix.converse(seq) << endl;
+
 
 	return 0;
 }
